269e5450bb1cc061a50b6d41e393d85373db6d7b
Iftakhar Husan <iftakharn19@gmail.com>
2025-02-23 07:28:06 +0200

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <main class="">
+    <h1>Home</h1>
+    <Button>tesr</Button>
+  </main>
+</template>
